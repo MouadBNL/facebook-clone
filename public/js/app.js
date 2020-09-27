c5730eddbab276ee3f88bc091e764b9f85b6209f
Mouad Benali <mouadbnl.2k01@gmail.com>
2020-09-27 14:13:41 +0100

    laravel echo and pusher js to required packages, and fixing boradcast config

--- public/js/app.js
+++ public/js/app.js
@@ -63450,7 +63450,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 
-window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js"); // Pusher.logToConsole = true;
+window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js"); //Pusher.logToConsole = true;
 
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
